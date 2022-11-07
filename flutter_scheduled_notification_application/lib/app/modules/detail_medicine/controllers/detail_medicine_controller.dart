f849a6406d1c556550e4bdf310c18ee320ccfc24
deanira <emailmilikdea@gmail.com>
2022-11-07 10:59:50 +0700

    add cancel notification when delete data

--- flutter_scheduled_notification_application/lib/app/modules/detail_medicine/controllers/detail_medicine_controller.dart
+++ flutter_scheduled_notification_application/lib/app/modules/detail_medicine/controllers/detail_medicine_controller.dart
@@ -2,21 +2,27 @@ import 'package:flutter_scheduled_notification_application/app/data/medicine.dar
 import 'package:flutter_scheduled_notification_application/app/data/notification.dart';
 import 'package:flutter_scheduled_notification_application/app/helper/db_helper.dart';
 import 'package:flutter_scheduled_notification_application/app/modules/home/controllers/home_controller.dart';
+import 'package:flutter_scheduled_notification_application/app/utils/notification_api.dart';
 import 'package:get/get.dart';
 
 class DetailMedicineController extends GetxController {
   var db = DbHelper();
   HomeController homeController = Get.put(HomeController());
+  late List<Notification> listNotification;
 
   Future<Medicine> getMedicineData(int id) async {
     return await db.queryOneMedicine(id);
   }
 
   Future<List<Notification>> getNotificationData(int id) async {
-    return await db.getNotificationsByMedicineId(id);
+    listNotification = await db.getNotificationsByMedicineId(id);
+    return listNotification;
   }
 
   void deleteMedicine(int id) async {
+    listNotification.forEach((element) {
+      NotificationApi.cancelNotification(element.id!);
+    });
     await db.deleteMedicine(id);
     await db.deleteNotificationByMedicineId(id);
 
